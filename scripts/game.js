5f8cdf012273b6141d3f46eb92d797b2f8cea7f3
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-09-30 22:08:21 +0200

    bruch

--- scripts/game.js
+++ scripts/game.js
@@ -11,21 +11,22 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload (){
     this.load.image('bg', 'assets/graphics/menubg.png');
     this.load.image('logo', 'assets/graphics/logo.png');
     this.load.image('particle', 'assets/graphics/particle.png');
 }
 
-function create ()
-{
+var logo, versionText;
+
+function create (){
     this.add.image(640, 360, 'bg');
     
     var particles = this.add.particles('particle');
@@ -36,12 +37,18 @@ function create ()
         blendMode: 'ADD'
     });
 
-    var logo = this.physics.add.image(400, 100, 'logo');
+    logo = this.physics.add.image(400, 100, 'logo');
 
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
 
     emitter.startFollow(logo);
-    
+
+    versionText = this.add.text(0, 0, 'janky alpha', {fontSize:'16px', fill:'#FFF'});   
+}
+
+function update (){
+    logo.rotation += 0.01;
+    versionText.setText('x ' + logo.x + '\ny ' + logo.y + "\nr " + logo.rotation);
 }
