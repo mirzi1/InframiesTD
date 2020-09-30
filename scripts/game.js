2010a3df380580e223d12aa0cdc6f9558a60edf4
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-09-30 21:07:04 +0200

    pozadie:)

---
+++ scripts/game.js
@@ -0,0 +1,46 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('bg', 'assets/graphics/menubg.png');
+    this.load.image('logo', 'assets/graphics/logo.png');
+    this.load.image('particle', 'assets/graphics/particle.png');
+}
+
+function create ()
+{
+    this.add.image(640, 360, 'bg');
+    /*
+    var particles = this.add.particles('particle');
+
+    var emitter = particles.createEmitter({
+        speed: 100,
+        scale: { start: 1, end: 0 },
+        blendMode: 'ADD'
+    });
+
+    var logo = this.physics.add.image(400, 100, 'logo');
+
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+
+    emitter.startFollow(logo);
+    */
+}
