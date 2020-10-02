9f778ad3e24d232abe2edcbd8a0786adf8ee2d58
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-10-02 13:31:03 +0200

    ťažké začiatky

--- scripts/game.js
+++ scripts/game.js
@@ -17,18 +17,71 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
+var graphics;
+var versionText;
+var background;
+var path;
 
-function preload (){
+var ENEMY_SPEED = 1/10000;
+
+function preload(){
+    //nacitanie contentu, lepsi bude spritesheet + json
     this.load.image('bg', 'assets/graphics/menubg.png');
     this.load.image('logo', 'assets/graphics/logo.png');
     this.load.image('particle', 'assets/graphics/particle.png');
 }
 
-var logo, versionText;
+var Enemy = new Phaser.Class({
+    Extends: Phaser.GameObjects.Image,
+    initialize:
+    function Enemy(scene){
+        Phaser.GameObjects.Image.call(this,scene,0,0,'particle');
+        this.follower = {t: 0, vec: new Phaser.Math.Vector2()};
+        this.hp = 0;
+    },
+    update: function(time, delta){
+        // move the t point along the path, 0 is the start and 0 is the end
+        this.follower.t += ENEMY_SPEED * delta;
+            
+        // get the new x and y coordinates in vec
+        path.getPoint(this.follower.t, this.follower.vec);
+        
+        // update enemy x and y to the newly obtained x and y
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
 
-function create (){
-    this.add.image(640, 360, 'bg');
+        // if we have reached the end of the path, remove the enemy
+        if (this.follower.t >= 1)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+    },
+    startOnPath: function (){
+        this.follower.t = 0;
+        path.getPoint(this.follower.t, this.follower.vec);
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+    }
+});
+
+function create(){
+    background = this.add.image(640, 360, 'bg');        //background bude vzdy naspodku
+    versionText = this.add.text(0, 0, 'inframiesTD', {fontSize:'16px', fill:'#FFF'}); 
+    graphics = this.add.graphics();                     //cesty
+
+    //path 1
+    path = this.add.path(0, 500);
+    path.lineTo(500, 500);
+    path.lineTo(300, 300);
+    path.lineTo(500, 100);
+    path.lineTo(1280, 100);
+
+    graphics.lineStyle(3, 0xaaaaaa);
+    path.draw(graphics);
+
+    enemies = this.add.group({classType: Enemy, runChildUpdate: true});
+    this.nextEnemy = 0;
     
+    /*
     var particles = this.add.particles('particle');
 
     var emitter = particles.createEmitter({
@@ -43,12 +96,32 @@ function create (){
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
 
-    emitter.startFollow(logo);
-
-    versionText = this.add.text(0, 0, 'janky alpha', {fontSize:'16px', fill:'#FFF'});   
+    emitter.startFollow(logo);*/
 }
 
-function update (){
-    logo.rotation += 0.01;
-    versionText.setText('x ' + logo.x + '\ny ' + logo.y + "\nr " + logo.rotation);
+function update(time, delta){
+    // if its time for the next enemy
+    if (time > this.nextEnemy)
+    {        
+        var enemy = enemies.get();
+        if (enemy)
+        {
+            enemy.setActive(true);
+            enemy.setVisible(true);
+            
+            // place the enemy at the start of the path
+            enemy.startOnPath();
+            
+            this.nextEnemy = time + 2000;
+        }       
+    }
+}
+
+function generateLevel(level){
+    switch(level){
+        case 1: //path = this.add.path(96,-32);
+        //path.lineTo(480,164);
+        versionText.setText('cc');break;
+    }
+    
 }
