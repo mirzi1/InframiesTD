423ea82fbd227b9fecb554922be13a76c465f3bf
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-11-01 15:13:17 +0100

    tower snusk

--- scripts/game.js
+++ scripts/game.js
@@ -4,10 +4,7 @@ var config = {
     height: 720,
     parent: 'main-game',
     physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 500 }
-        }
+        default: 'arcade'
     },
     scene: {
         preload: preload,
@@ -24,12 +21,20 @@ var background_main;
 var background;
 var path;
 
-//var animFrame = 0;
-
 var VERSION = "v0.1"
 var LIVES = 100;
 var MONEY = 100;
 var ENEMY_SPEED = 1/10000;
+var BULLET_DAMAGE = 30;
+
+var level1 =       [[ -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, 0],
+                    [ -1, 0, 0,-1,-1,-1, 0,-1, 0,-1,-1, 0, 0],
+                    [ -1, 0, 0,-1,-1,-1, 0,-1, 0,-1,-1, 0, 0],
+                    [ -1, 0, 0,-1,-1,-1, 0, 0, 0,-1,-1, 0, 0],
+                    [ -1, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1, 0, 0],
+                    [ -1, 0, 0,-1,-1,-1,-1,-1,-1,-1,-1, 0, 0],
+                    [ -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [ -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1]];
 
 function preload(){
     //nacitanie spritov
@@ -93,49 +98,118 @@ var Enemy = new Phaser.Class({
     },
     startOnPath:
     function(){
+        this.hp = 100;
         this.follower.t = 0;
         path.getPoint(this.follower.t, this.follower.vec);
         this.setPosition(this.follower.vec.x, this.follower.vec.y);
+    },
+    receiveDamage:
+    function(damage) {
+        this.hp -= damage;
+
+        // if hp drops below 0 we deactivate this enemy
+        if(this.hp <= 0) {
+            this.destroy();
+        }
     }
 });
 
+var Turret = new Phaser.Class({
+    Extends: Phaser.GameObjects.Sprite,
+
+    initialize:
+
+        function Turret (scene)
+        {
+            Phaser.GameObjects.Sprite.call(this, scene, 0, 0, 't1');
+            this.nextTic = 0;
+        },
+    place: function(i, j) {
+        this.y = i * 100 + 100/2;
+        this.x = j * 100 + 100/2;
+        level1[i][j] = 1;
+    },
+    fire: function() {
+        var enemy = getEnemy(this.x, this.y, 200);
+        if(enemy) {
+            var angle = Phaser.Math.Angle.Between(this.x, this.y, enemy.x, enemy.y);
+            addBullet(this.x, this.y, angle);
+            this.angle = (angle + Math.PI/2) * Phaser.Math.RAD_TO_DEG;
+            this.play('t1_fire');
+        }
+    },
+    update: function (time, delta)
+    {
+        if(time > this.nextTic) {
+            this.fire();
+            this.nextTic = time + 1000;
+        }
+    }
+});
+
+var Bullet = new Phaser.Class({
+
+    Extends: Phaser.GameObjects.Sprite,
+
+    initialize:
+
+        function Bullet (scene)
+        {
+            Phaser.GameObjects.Sprite.call(this, scene, 0, 0, 'particle');
+
+            this.incX = 0;
+            this.incY = 0;
+            this.lifespan = 0;
+
+            this.speed = Phaser.Math.GetSpeed(600, 1);
+        },
+
+    fire: function (x, y, angle)
+    {
+        this.setActive(true);
+        this.setVisible(true);
+        //  Bullets fire from the middle of the screen to the given x/y
+        this.setPosition(x, y);
+
+        //  we don't need to rotate the bullets as they are round
+        //    this.setRotation(angle);
+
+        this.dx = Math.cos(angle);
+        this.dy = Math.sin(angle);
+
+        this.lifespan = 1000;
+    },
+
+    update: function (time, delta)
+    {
+        this.lifespan -= delta;
+
+        this.x += this.dx * (this.speed * delta);
+        this.y += this.dy * (this.speed * delta);
+
+        if (this.lifespan <= 0)
+        {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+    }
+
+});
+
 function create(){
-    background_main = this.add.image(640, 360, 'bg');        //background bude vzdy naspodku
-    background = this.add.image(715, 415, 'bg1');        //background bude vzdy naspodku
-    versionText = this.add.text(0, 0, 'inframiesTD ' + VERSION, {fontSize:'16px', fill:'#FFF'});
-    infoText = this.add.text(20, 20, 'HP: ' + LIVES + '\nCASH: ' + MONEY, {fontSize:'30px', fill:'#0FF'});
-    graphics = this.add.graphics();                     //cesty
+    background_main = this.add.image(640, 360, 'bg');       //background bude vzdy naspodku
+    background = this.add.image(715, 415, 'bg1');           //background bude vzdy naspodku
+    versionText = this.add.text(0, 0, 'inframiesTD ' + VERSION, {fontSize:'16px', fill:'#FFF'});                //misc text
+    infoText = this.add.text(20, 20, 'HP: ' + LIVES + '\nCASH: ' + MONEY, {fontSize:'30px', fill:'#0FF'});      // -||-
+    graphics = this.add.graphics();                         //cesty
 
-    //generovanie animacii (dlhy chunk kodu)
-    this.anims.create({
-        key: "a2_normal",
-        frameRate: 15,
-        frames: this.anims.generateFrameNumbers("a2",{start:0, end:9}),
-        repeat: -1
-    });
-    this.anims.create({
-        key: "t1_fire",
-        frameRate: 15,
-        frames: this.anims.generateFrameNumbers("t1",{start:0, end:8}),
-        repeat: -1  //TODO: zmen vsade na 0 ked bude kod pre towery pridany
-    });
-    this.anims.create({
-        key: "t2_fire",
-        frameRate: 15,
-        frames: this.anims.generateFrameNumbers("t2",{start:0, end:9}),
-        repeat: -1
-    });
-    this.anims.create({
-        key: "t3_fire",
-        frameRate: 15,
-        frames: this.anims.generateFrameNumbers("t3",{start:0, end:10}),
-        repeat: -1
-    });
-
-    //TODO: odstran ked bude hotovy kod pre towery
-    this.add.sprite(200,200,'t1').play('t1_fire');
-    this.add.sprite(700,350,'t2').play('t2_fire');
-    this.add.sprite(1230,500,'t3').play('t3_fire').setFlip(true);
+    //generovanie animacii
+    //attackers
+    this.anims.create({key: "a2_normal", frameRate: 15, frames: this.anims.generateFrameNumbers("a2",{start:0, end:9}), repeat: -1});
+    //towers
+    this.anims.create({key: "t1_fire", frameRate: 15, frames: this.anims.generateFrameNumbers("t1",{start:8, end:0}), repeat: 0});
+    this.anims.create({key: "t2_fire", frameRate: 15, frames: this.anims.generateFrameNumbers("t2",{start:0, end:9}), repeat: 0});
+    this.anims.create({key: "t3_fire", frameRate: 15, frames: this.anims.generateFrameNumbers("t3",{start:0, end:10}), repeat: 0});
 
     //path 1
     path = this.add.path(320, 110);
@@ -149,9 +223,14 @@ function create(){
     graphics.lineStyle(3, 0xaaaaaa);
     path.draw(graphics);
 
-    enemies = this.add.group({classType: Enemy, runChildUpdate: true});
+    turrets = this.add.group({ classType: Turret, runChildUpdate: true });
+    bullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
+    enemies = this.physics.add.group({ classType: Enemy, runChildUpdate: true });
+
+    this.physics.add.overlap(enemies, bullets, damageEnemy);
+    this.input.on('pointerdown', placeTurret);
     this.nextEnemy = 0;
-    
+
     /*
     var particles = this.add.particles('particle');
 
@@ -171,7 +250,7 @@ function create(){
 }
 
 function update(time, delta){
-    // if its time for the next enemy
+    // spawn utocnika podla arrayu kazdych n milisekund
     if (time > this.nextEnemy){        
         var enemy = enemies.get();
         if (enemy)
@@ -179,19 +258,58 @@ function update(time, delta){
             enemy.setActive(true);
             enemy.setVisible(true);
             
-            // place the enemy at the start of the path
+            // ulozenie utocnika na zaciatok
             enemy.startOnPath();
             
             this.nextEnemy = time + 2000;
         }   
     }
-    /*
-    //oppa yandev style
-    if(time%5==0){
-    animFrame++;
-    if(animFrame>=10)animFrame=0;  
-    }
-    */
 
 }
-    
+
+function placeTurret(pointer) {
+    var i = Math.floor(pointer.y/100);
+    var j = Math.floor(pointer.x/100);
+    if(canPlaceTurret(i, j)) {
+        var turret = turrets.get();
+        if (turret)
+        {
+            turret.setActive(true);
+            turret.setVisible(true);
+            turret.place(i, j);
+        }
+    }
+}
+
+function canPlaceTurret(i, j) {
+    return level1[i][j] === 0;
+}
+
+function getEnemy(x, y, distance) {
+    var enemyUnits = enemies.getChildren();
+    for(var i = 0; i < enemyUnits.length; i++) {
+        if(enemyUnits[i].active && Phaser.Math.Distance.Between(x, y, enemyUnits[i].x, enemyUnits[i].y) < distance)
+            return enemyUnits[i];
+    }
+    return false;
+}
+
+function addBullet(x, y, angle) {
+    var bullet = bullets.get();
+    if (bullet)
+    {
+        bullet.fire(x, y, angle);
+    }
+}
+
+function damageEnemy(enemy, bullet) {
+    // only if both enemy and bullet are alive
+    if (enemy.active === true && bullet.active === true) {
+        // we remove the bullet right away
+        bullet.setActive(false);
+        bullet.setVisible(false);
+
+        // decrease the enemy hp with BULLET_DAMAGE
+        enemy.receiveDamage(BULLET_DAMAGE);
+    }
+}
