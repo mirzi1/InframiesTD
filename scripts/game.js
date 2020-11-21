26d7fac936e420c7f77b93e27237cb372a97a05a
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-11-22 00:37:56 +0100

    oop

--- scripts/game.js
+++ scripts/game.js
@@ -210,7 +210,7 @@ function preload(){
     this.load.spritesheet('freespace', 'assets/graphics/ui/freespace.png' ,{frameHeight: 50, frameWidth: 50});
 
     //pozadia
-    this.load.image('bg1', 'assets/graphics/levels/bg1.png);
+    this.load.image('bg1', 'assets/graphics/levels/bg1.png');
     this.load.image('bg2', 'assets/graphics/levels/bg2.jpeg');
     this.load.image('bg3', 'assets/graphics/levels/bg3.png');
 
