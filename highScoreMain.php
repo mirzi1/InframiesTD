d64750f1aa3c581559658a167f30bee8321cc1d5
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-12-14 19:45:30 +0100

    wooo yeah baby that's what i've been waiting for that's what it's all about

--- highScoreMain.php
+++ highScoreMain.php
@@ -2,15 +2,16 @@
 <html lang="en" dir="ltr">
 <head>
     <meta charset="utf-8">
-    <title>DB vypisky</title>
+    <title>HALL OF FAME</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<h1 class='title'>HALL OF FAME</h1>
 <table class="main_table">
     <tr>
-        <th class="table_th">Pos.</th>
-        <th class="table_th">Username</th>
-        <th class="table_th">Score</th>
+        <th class="table_th">POS</th>
+        <th class="table_th">NAME</th>
+        <th class="table_th">SCORE</th>
     </tr>
     <?php
     $conn = mysqli_connect("localhost", "root", "", "highscore");
@@ -18,7 +19,7 @@
         die("Connection failed: ". $conn-> connect_error);
     }
 
-    $sql = "SELECT meno, score FROM hs ORDER BY score DESC LIMIT 10";
+    $sql = "SELECT meno, score FROM hs ORDER BY score DESC LIMIT 50";
     $result = $conn -> query($sql);
 
     if ($result -> num_rows > 0) {
@@ -35,5 +36,7 @@
     $conn -> close();
     ?>
 </table>
+
+<p class='center'>THANKS FOR PLAYING INFRAMIESTD!</p>
 </body>
 </html>
