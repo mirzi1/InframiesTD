dbc1ad5673ba221a0760541681dc8984904db9a2
lengyel <miroslav.lengyel@student.ukf.sk>
2021-04-24 19:14:23 +0200

    a, s and f keys are a cool speedrunning tool now

--- scripts/game.js
+++ scripts/game.js
@@ -2157,6 +2157,9 @@ function createGame(){
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.EIGHT).on('down', function() {changeSelectedTower(8)}, this);
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.NINE) .on('down', function() {upgradeTool()}, this);
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ZERO) .on('down', function() {sellTool()}, this);
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)    .on('down', function() {upgradeTool()}, this);
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)    .on('down', function() {sellTool()}, this);
+    this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)    .on('down', function() {if(!waveInProgress && canGoToNextLevel)nextWave()}, this);
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE).on('down', function() {if(!waveInProgress && canGoToNextLevel)nextWave()}, this);
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F)    .on('down', function() {toggleFullscreen()}, this);
     this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.N)    .on('down', function() {toggleSound()}, this);
